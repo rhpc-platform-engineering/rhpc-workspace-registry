03a98931cf073d64c80cde439fcb79695d45c737
robust-zul <zulficar@robusthpc.com>
2024-07-31 19:17:25 +0800

    Update basePath

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/rhpc-workspace-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
